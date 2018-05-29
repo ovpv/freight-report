25a5ed61767b49249695334a6a13c0eb5869470c
ovpv <viktor.prasad@gmail.com>
2018-05-29 14:06:22 +0530

    :pencil2: minor fix

--- pages/layouts/page.js
+++ pages/layouts/page.js
@@ -1,18 +1,10 @@
 import { Component } from "react";
 import Header from '../components/header';
-import {NextScript} from 'next/document';
 
 export default class Page extends Component{
     render(){
         return(
             <div>
-                <style global jsx>
-                    {`
-                        body{
-                            background-color:#E0E0E0;
-                        }
-                    `}
-                </style>
                 <Header/>
                 <main>
                     {this.props.children}
